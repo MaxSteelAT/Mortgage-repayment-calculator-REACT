fced83b00c2be76f382ea5d9554150f3aaf0e864
Max Steel <mtra.ingrid.lopez@gmail.com>
2025-02-17 11:27:03 -0800

    Se creó el proyecto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './index.css';
+import './reset.css';
+
+
+const App = () => {
+
+  return (
+    <div className="App">
+    <h3>Prueba</h3>
+    </div>
+  );
+}
+
+export default App;
