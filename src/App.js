230ac69bee8ec2f6e59b4994d3d325623aae723b
Max Steel <mtra.ingrid.lopez@gmail.com>
2025-03-12 12:24:43 -0700

    El proyecto se hizó responsivo

--- src/App.js
+++ src/App.js
@@ -15,13 +15,41 @@ const App = () => {
 
   const [showResult, setShowResult] = useState(true)
 
+  const [amountError, setAmountError] = useState(false)
+  const [termError, setTermError] = useState(false)
+  const [rateError, setRatetError] = useState(false)
 
   const calculate = (event) => {
     event.preventDefault()
-    console.log(selectRadio)
-    const months = calculateMonths(inputTerm)
 
+    const months = calculateMonths(inputTerm)
     const rate = monthlyRate(inputRate)
+    let hasError = false
+
+    if (inputAmount <= 0 || inputAmount == '' || isNaN(inputAmount)) {
+      setAmountError(true)
+      hasError = true
+    } else {
+      setAmountError(false)
+    }
+
+    if (inputTerm <= 0 || inputTerm == '' || isNaN(inputTerm)) {
+      setTermError(true)
+      hasError = true
+    } else {
+      setTermError(false)
+    }
+
+    if (inputRate <= 0 || inputRate == '' || isNaN(inputRate)) {
+      setRatetError(true)
+      hasError = true
+    } else {
+      setRatetError(false)
+    }
+
+    if (hasError == true) {
+      return;
+    }
 
     if (selectRadio == 'option1') {
       const monthlyPayment = inputAmount * (rate * Math.pow(1 + rate, months)) / (Math.pow(1 + rate, months) - 1);
@@ -30,9 +58,9 @@ const App = () => {
       const refund = monthlyPayment * months
       setTotalTerm(Number(refund).toFixed(2))
     }
+
     if (selectRadio == 'option2') {
       const monthlyPayment = inputAmount * rate;
-      setMonthlyRepayment(Number(monthlyPayment).toFixed(2))
 
       const total = monthlyPayment * 12;
       setInterest(Number(total).toFixed(2))
@@ -86,20 +114,32 @@ const App = () => {
             <span className="input-group-text" id="basic-addon1">£</span>
             <input onChange={changeAmount} value={inputAmount} type="number" className="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" />
           </div>
+          {amountError && (
+            <h3 className="input-error">
+              This field is required
+            </h3>)}
           <div className="row">
-            <div className="col">
+            <div className="col-sm-12 col-md-6">
               <label>Mortgage Term</label>
               <div className="input-group input-group-border">
                 <input onChange={changeTerm} value={inputTerm} type="number" className="form-control" aria-label="Dollar amount (with dot and two decimal places)" />
                 <span className="input-group-text">years</span>
               </div>
+              {termError && (
+                <h3 className="input-error">
+                  This field is required
+                </h3>)}
             </div>
-            <div className="col">
+            <div className="col-sm-12 col-md-6">
               <label>Interest Rate</label>
               <div className="input-group input-group-border">
                 <input onChange={changeRate} value={inputRate} type="number" className="form-control" aria-label="Dollar amount (with dot and two decimal places)" />
                 <span className="input-group-text">%</span>
               </div>
+              {rateError && (
+                <h3 className="input-error">
+                  This field is required
+                </h3>)}
             </div>
           </div>
           <label>Mortgage Type</label>
@@ -120,14 +160,12 @@ const App = () => {
             Calculate Repayments
           </button>
         </form>
-      </div >
-      {showResult === true ? (
+      </div>
+      {showResult ? (
         <div className="results-shows">
-          <div className="resultTemplate">
-            <img src={image} />
-            <h4 className="result-text">Result show here</h4>
-            <p className="paragraph">Complet the form and click "calculate repayments" to see what your monthly repayments would be.</p>
-          </div>
+          <img src={image} />
+          <h4 className="result-text">Result show here</h4>
+          <p className="paragraph">Complet the form and click "calculate repayments" to see what your monthly repayments would be.</p>
         </div>
       ) : (
         <div className="your-result">
@@ -159,7 +197,8 @@ const App = () => {
               </div>
             </div>
           )}
-        </div>)}
+        </div>
+      )}
     </div>
   );
 }
